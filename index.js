3802ea14e5774a195e402d4c3fa8bd5fcae417c3
nbkhope <ryumemaru@yahoo.com>
2016-09-23 18:38:21 -0700

    Add basic server setup

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 // App Setup
+const app = express();
 
 // Server Setup
+const PORT = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(PORT);
+console.log("Server listening on port", PORT);
