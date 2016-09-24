f3c33387d438717bd3aa65fa25812e992687417b
nbkhope <ryumemaru@yahoo.com>
2016-09-23 18:41:04 -0700

    Use morgan and bodyParser middleware

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ const morgan = require('morgan');
 // App Setup
 const app = express();
 
+// For logging
+app.use(morgan('combined'));
+// Parse incoming requests as though they were json
+app.use(bodyParser.json({ type: '*/*' }));
+
 // Server Setup
 const PORT = process.env.PORT || 3090;
+// HTTP Server
 const server = http.createServer(app);
 server.listen(PORT);
 console.log("Server listening on port", PORT);
